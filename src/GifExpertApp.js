458a5751fd2f9e4356519f6f3c5aabf586083a40
Theiler Braian <braian_theiler@hotmail.com>
2020-11-18 11:32:40 -0300

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+
+    const [categories, setcategories] = useState(['Electricidad']);
+
+    return (
+        <>
+            <h2> GifExpertApp</h2>
+            <AddCategory setCategories={setcategories}/>
+            <hr/> 
+
+            <ol>
+                {
+                    categories.map( category => (
+                        <GifGrid category = { category } 
+                        key = {category}
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
